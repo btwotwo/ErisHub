3f33cb1634ac896d538609929829efcd8e3121b0
bo20202 <bo20202@yandex.ru>
2021-02-28 22:46:21 +0100

    Remove garbage, move classes to different files

--- ErisHub/Configuration/Server.cs
+++ ErisHub/Configuration/Server.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using ErisHub.Configuration.Webhook;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,33 +14,5 @@ namespace ErisHub.Configuration
         public int Port { get; set; }
 
         public WebhookConfiguration WebhookConfiguration { get; set; }
-    }
-
-    public interface IWebhookConfig
-    {
-        Uri Url { get; set; }
-    }
-
-    public class RestartWebhookConfig: IWebhookConfig
-    {
-        public Uri Url { get; set; }
-        public ulong RoleId { get; set; }
-    }
-
-    public class LobbyAlert: IWebhookConfig
-    {
-        public Uri Url { get; set; }
-
-    }
-
-    public class WebhookConfiguration
-    {
-        public RestartWebhookConfig RestartWebhook{ get; set; }
-        public Uri LobbyAlert { get; set; }
-        public Uri AdminHelp { get; set; }
-
-        public Uri AdminAlert { get; set; }
-        public Uri CoderAlert { get; set; }
-        public Uri AdmiraltyAlert { get; set; }
     }
 }
